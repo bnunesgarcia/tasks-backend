93e2f92bb383b13bb9f1b01062b1d2c2fce08595
Bruno Garcia <brung@br.ibm.com>
2023-10-20 16:09:19 -0300

    atualizacao de code

--- src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -2,32 +2,73 @@ package br.ce.wcaquino.taskbackend.controller;
 
 import java.time.LocalDate;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import br.ce.wcaquino.taskbackend.model.Task;
 import br.ce.wcaquino.taskbackend.utils.ValidationException;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
 
 public class TaskControllerTest {
 	
+	@Mock
+	private TaskRepo TaskRepo;
+
+	@InjectMocks
+	private TaskController controller;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
 	@Test
-	public void naoDeveSalvarTarefaSemDescricao() throws ValidationException {
+	public void naoDeveSalvarTarefaSemDescricao() {
 		Task todo = new Task();
 		todo.setDueDate(LocalDate.now());
-		TaskController controller = new TaskController();
-		controller.save(todo);
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar neste ponto!");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the task description", e.getMessage());
+		}
 	}
 	
 	@Test
 	public void naoDeveSalvarTarefaSemData() {
-		
+		Task todo = new Task();
+		todo.setTask("Descrição");
+//		todo.setDueDate(LocalDate.now());
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar neste ponto!");
+		} catch (ValidationException e) {
+			 Assert.assertEquals("Fill the due date", e.getMessage());
+		}
 	}
 	
 	@Test
 	public void naoDeveSalvarTarefaComDataPassada() {
-		
+		Task todo = new Task();
+		todo.setTask("Descrição");
+		todo.setDueDate(LocalDate.of(2023, 01, 29));
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar neste ponto!");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Due date must not be in past", e.getMessage());
+		}
 	}
 	
 	@Test
-	public void deveSalvarTarefaComSucesso() {
-		
-	}
+	public void deveSalvarTarefaComSucesso() throws ValidationException {
+		Task todo = new Task();
+		todo.setTask("Descrição");
+		todo.setDueDate(LocalDate.now());
+		controller.save(todo);
+	}	
 }
